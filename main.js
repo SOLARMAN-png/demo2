0d674ee4937625d42d5127f7b563c6f699e5a16d
iamSOLARMAN <sanusitaiwo123@gmail.com>
2025-06-30 10:42:12 +0100

    Add LICENSE

---
+++ main.js
@@ -0,0 +1,10 @@
+let demo = function() {
+    // This function is a placeholder for any JavaScript code you want to run.
+    console.log("JavaScript is running!");
+    
+    // Example: Change the background color of the body
+    document.body.style.backgroundColor = "lightblue";
+    
+    // Example: Log a message to the console
+    console.log("Welcome to the demo page!");
+};
